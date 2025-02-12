80caefff3d77e60b1fdf1e44250d33f52458bea3
Adrian-ror <adrian.rodriguez10@udc.es>
2025-02-12 18:38:57 +0100

    Arreglo

--- TaskLog/src/modules/home/components/Home.jsx
+++ TaskLog/src/modules/home/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <div className="grid px-16 pt-20  mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12 ">
           <TitleDescription />
           <div className=" hidden mr-8 lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="./home.png" alt="home image" />
+            <img src="/public/home.png" alt="home image" />
           </div>
         </div>
       </section>
